7b31d230324a967d8dff41cb61c6c371eb932f90
Ryan Taylor <ryanmt@byu.net>
2013-01-05 21:31:42 -0700

    init

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  def home
+    @tmp = nil
+  end
+  def story
+  end
+  def photos
+  end
+  def registry
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JandrCom::Application.config.secret_token = 'a90e42fcee2bff6fb7fd1eb8c283624758892abac622b755101680224723c8513279c78cefd3d8acff30eac09dfaaa550fd52a65d4410d62148c7bad486f0ad0'
