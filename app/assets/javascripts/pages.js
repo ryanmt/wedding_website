99df64930f3ff3b9d302baf3fd7f67360932020a
Ryan Taylor <ryanmt@byu.net>
2013-01-17 13:09:40 -0500

    fixes for GA

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -3,17 +3,6 @@
 //# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
 //= require slides.min.jquery
-//# GOOGLE ANALYTICS
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-37760713-1']);
-_gaq.push(['_setDomainNAme', 'janiceandryan.org']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
 
 //# SLIDES LOADING
 $(function(){
